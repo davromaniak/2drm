140528b5f544c9618111ba07c3bcb51899beff75
Cyril LAVIER <cyril.lavier+github@davromaniak.eu>
2011-10-21 22:47:16 +0200

    first commit

---
+++ css/js.js
@@ -0,0 +1,8 @@
+function toggle(obj) {
+        var e = document.getElementById(obj);
+        if (e.style.display == "none") {
+                e.style.display = "";
+        } else {
+                e.style.display = "none";
+        }
+} 
